d75c29af26981ad718fd3a5d5c64e630e0c6aa84
tanushka-102 <124056303+tanushka-102@users.noreply.github.com>
2025-07-13 21:17:23 +0530

    Update file_utils.py

--- utils/file_utils.py
+++ utils/file_utils.py
@@ -1,1 +1,16 @@
+import pdfplumber
+
+def extract_text(uploaded_file):
+    if uploaded_file.type == "application/pdf":
+        text = ""
+        with pdfplumber.open(uploaded_file) as pdf:
+            for page in pdf.pages:
+                page_text = page.extract_text()
+                if page_text:
+                    text += page_text + "\n"
+        return text
+    elif uploaded_file.type == "text/plain":
+        return uploaded_file.read().decode("utf-8")
+    else:
+        return "Unsupported file format."
 
